5824c3ff99fdea76f183b820a7ab6576e1895a52
renzobautista <renzo@cmu.edu>
2014-05-24 22:08:19 -0500

    rush model

--- portal/models.py
+++ portal/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Brother(models.Model):
 	first_name = models.CharField(max_length=40)
@@ -16,3 +17,17 @@ class Brother(models.Model):
 
 	def __unicode__(self):
 		return self.first_name + " " + self.last_name
+
+class Rushee(models.Model):
+	user = models.OneToOneField(User)
+	phone_num = models.CharField(max_length=20)
+	dorm = models.CharField(max_length=40)
+	grad_class = models.CharField(max_length=20)
+	major = models.CharField(max_length=100)
+	gpa = models.CharField(max_length=20)
+	q1 = models.TextField(blank=True)
+	q2 = models.TextField(blank=True)
+	q3 = models.TextField(blank=True)
+	q4 = models.TextField(blank=True)
+	picture = models.ImageField(upload_to="rushpics")
+	resume = models.FileField(upload_to="rushresumes")
